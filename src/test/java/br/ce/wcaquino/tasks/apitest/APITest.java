43268a995ca63a50ce2565dc897171a25b898b29
vscalado <vitor.santos.calado@gmail.com>
2020-05-23 17:22:17 -0300

    Primeiro Commit

---
+++ src/test/java/br/ce/wcaquino/tasks/apitest/APITest.java
@@ -0,0 +1,53 @@
+package br.ce.wcaquino.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setUp() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+	}
+	@Test
+	public void deveRetornarTarefas() {
+		RestAssured.given()
+			
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200)
+			;
+	}
+	
+	@Test
+	public void deveAdicionarTarefasComSucesso() {
+		RestAssured.given()
+			.body("{\"task\": \"Teste via RestAssured\", \"dueDate\": \"2020-12-30\"}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.log().all()
+			.statusCode(201)
+			;
+	}
+	
+	@Test
+	public void deveAdicionarTarefasSemSucesso() {
+		RestAssured.given()
+			.body("{\"task\": \"Teste via RestAssured\", \"dueDate\": \"2010-12-30\"}")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.log().all()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+			;
+	}
+}
